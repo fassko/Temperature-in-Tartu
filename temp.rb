362b23fc693e4360f863d986e460a1c8e1384b24
Kristaps Grinbergs <kristaps@kristaprinbergs.lan>
2014-01-20 17:55:15 +0200

    Using city ID

--- temp.rb
+++ temp.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'json'
 
-result = JSON.parse(open("http://api.openweathermap.org/data/2.5/weather?q=Tartu,Estonia&units=metric").read)
+result = JSON.parse(open("http://api.openweathermap.org/data/2.5/weather?id=588335&units=metric").read)
 
 puts result["main"]["temp"].to_f.round(2)
